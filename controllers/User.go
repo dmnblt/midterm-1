e2a67ccb662b1eb0642eaadfcbea847acd9c806b
Duman <baltabayduman@mail.ru>
2023-03-17 22:32:40 +0600

    fix json password

--- controllers/User.go
+++ controllers/User.go
@@ -14,7 +14,7 @@ type User struct {
 	ID        int64  `json:"id"`
 	Name      string `json:"name"`
 	Email     string `json:"email"`
-	Password  string `json:"-"`
+	Password  string `json:"password"`
 	CreatedAt string `json:"created_at"`
 }
 
