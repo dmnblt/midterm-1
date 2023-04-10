7f4a6c1a175d9843321f0f804bf5869f102a71e1
meiirzhan <meiirzhan.uristemov@gmail.com>
2023-04-10 17:17:47 +0600

    added feature for changing user password

--- controllers/User.go
+++ controllers/User.go
@@ -196,3 +196,32 @@ func UpdateUserEmail(c *gin.Context) {
 	user.Email = update.Email
 	c.JSON(http.StatusOK, user)
 }
+func UpdateUserPassword(c *gin.Context) {
+	userID := c.Param("id")
+
+	var user User
+	query := "SELECT * FROM users WHERE id = ?"   err := db.QueryRow(query, userID).Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "User not found",      })
+		return   }
+
+	var update struct {
+		Password string `json:"password" binding:"required"`   }
+	if err := c.ShouldBindJSON(&update); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request payload",      })
+		return   }
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(update.Password), 10)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
+		return   }
+	query = "UPDATE users SET password = ? WHERE id = ?"   _, err = db.Exec(query, hashedPassword, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to update user email",      })
+		return   }
+
+	user.Password = hashedPassword
+	c.JSON(http.StatusOK, user)
+}
