bca94f20cabe3ef571d55b991850435c9bb46266
Duman <baltabayduman@mail.ru>
2023-04-23 19:33:30 +0600

    added report 10

--- controllers/News.go
+++ controllers/News.go
@@ -36,3 +36,47 @@ func AddNews(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "news added successfully"})
 }
+
+func deleteNews(c *gin.Context) {
+	id := c.Param("id") // get the id of the news item to be deleted from the URL parameter
+	var news News
+	db := dbConnect() // get a database connection
+	if err := db.Where("id = ?", id).Delete(&news).Error; err != nil {
+		// handle error if any
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "News item deleted successfully"})
+}
+
+func updateNews(c *gin.Context) {
+	id := c.Param("id")
+
+	var news News
+	db := dbConnect()
+
+	// Find the news item in the database by its ID
+	if err := db.First(&news, id).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "News item not found"})
+		return
+	}
+
+	// Parse the JSON request body into a News struct
+	var updatedNews News
+	if err := c.ShouldBindJSON(&updatedNews); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON payload"})
+		return
+	}
+
+	// Update the news item in the database
+	news.IconURL = updatedNews.IconURL
+	news.Title = updatedNews.Title
+	news.Body = updatedNews.Body
+
+	if err := db.Save(&news).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": news})
+}
